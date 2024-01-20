ebe0f8631d2218505b9158eb539034b7d469dc29
codyivy0 <codyivy0@gmail.com>
2024-01-19 23:38:56 -0600

    scorekeeping and reset

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import styles from "./Buttons.module.scss";
 
 const Buttons = () => {
-  const [message, setMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
   const [enemy, setEnemy] = useState("");
   const [color, setColor] = useState("");
+  const [playerScore, setPlayerScore] = useState(0);
+  const [enemyScore, setEnemyScore] = useState(0);
 
   function handleClick(playerChoice) {
     setEnemy("");
@@ -41,24 +43,30 @@ const Buttons = () => {
             setMessage("This is a tie!");
           } else if (enemyChoice === 2) {
             setMessage("You Win!");
+            setPlayerScore(prev => prev + 1)
           } else if (enemyChoice === 3) {
             setMessage("You lose!");
+            setEnemyScore(prev => prev + 1)
           }
           break;
         case "rock":
           if (enemyChoice === 1) {
             setMessage("You lose!");
+            setEnemyScore(prev => prev + 1)
           } else if (enemyChoice === 2) {
             setMessage("This is a tie!");
           } else if (enemyChoice === 3) {
             setMessage("You win!");
+            setPlayerScore(prev => prev + 1)
           }
           break;
         case "scissors":
           if (enemyChoice === 1) {
             setMessage("You win!");
+            setPlayerScore(prev => prev + 1)
           } else if (enemyChoice === 2) {
             setMessage("You lose!");
+            setEnemyScore(prev => prev + 1)
           } else if (enemyChoice === 3) {
             setMessage("This is a tie!");
           }
@@ -71,39 +79,58 @@ const Buttons = () => {
       }
     }, 1000);
   }
+
+  function handleReset() {
+    setMessage('')
+    setEnemy('')
+    setColor('')
+    setEnemyScore(0)
+    setPlayerScore(0)
+  }
+
   return (
     <>
-      <div className={styles.gameButtonContainer}>
-        <div className={styles.top}>
-          <img
-            className={styles.gameButton}
-            onClick={() => handleClick(`paper`)}
-            src="/paper.png"
-            alt=""
-          />
+      <section className={styles.gameContainer}>
+        <div className={styles.infoBar}>
+          <p>Your Score: {playerScore}</p>
+          <p>Enemy Score: {enemyScore}</p>
         </div>
-        <div className={styles.bottom}>
-          <img
-            className={styles.gameButton}
-            onClick={() => handleClick(`rock`)}
-            src="/rock.png"
-            alt=""
-          />
-          <img
-            className={styles.gameButton}
-            onClick={() => handleClick(`scissors`)}
-            src="/scissors.png"
-            alt=""
-          />
+        <div className={styles.gameButtonContainer}>
+          <div className={styles.top}>
+            <img
+              className={styles.gameButton}
+              onClick={() => handleClick(`paper`)}
+              src="/paper.png"
+              alt=""
+            />
+          </div>
+          <div className={styles.bottom}>
+            <img
+              className={styles.gameButton}
+              onClick={() => handleClick(`rock`)}
+              src="/rock.png"
+              alt=""
+            />
+            <img
+              className={styles.gameButton}
+              onClick={() => handleClick(`scissors`)}
+              src="/scissors.png"
+              alt=""
+            />
+          </div>
         </div>
-      </div>
-      {isLoading && <h2 className={styles.textCenter}>Loading...</h2>}
-      {enemy && (
-        <h2 className={styles.textCenter}>
-          The CPU chose <span style={{ color: color }}>{enemy}</span>
-        </h2>
-      )}
-      {message && <h1 className={styles.textCenter}>{message}</h1>}
+
+        {isLoading && <h2 className={styles.textCenter}>Loading...</h2>}
+        {enemy && (
+          <h2 className={styles.textCenter}>
+            The CPU chose <span style={{ color: color }}>{enemy}</span>
+          </h2>
+        )}
+        {message && <h1 className={styles.textCenter}>{message}</h1>}
+        <button className={styles.resetBtn} onClick={() => handleReset()}>
+          RESET
+        </button>
+      </section>
     </>
   );
 };
